00162b8b9173c86953950debc89a30f9244edb0b
Bruno Dias <brunodg.dev@gmail.com>
2022-09-26 19:48:08 -0300

    feat: Cannon and player character are moving now. On to Shooting and enemy Spawn.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private CharacterController _controller;
+    public Joystick joystick;
 
     [SerializeField]
     private float _playerSpeed = 20f;
 
     [SerializeField]
     private float _rotationSpeed = 10f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    [Header("Trash Collected")]
+    [SerializeField] private int organicAmmo;
+    [SerializeField] private int metalAmmo;
+    [SerializeField] private int plasticAmmo;
 
     private void Start()
     {
@@ -24,8 +33,29 @@ public class PlayerControl : MonoBehaviour
 
     void Movement()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if (joystick.Horizontal >= .2f)
+        {
+            horizontalInput = _playerSpeed;
+        } else if (joystick.Horizontal <= -.2f)
+        {
+            horizontalInput = -_playerSpeed;
+        } else
+        {
+            horizontalInput = 0f;
+        }
+
+        if (joystick.Vertical >= .2f)
+        {
+            verticalInput = _playerSpeed;
+        }
+        else if (joystick.Vertical <= -.2f)
+        {
+            verticalInput = -_playerSpeed;
+        }
+        else
+        {
+            verticalInput = 0f;
+        }
 
         Vector3 movementInput = new Vector3(horizontalInput, 0, verticalInput);
         Vector3 movementDirection = movementInput.normalized;
@@ -38,5 +68,28 @@ public class PlayerControl : MonoBehaviour
         }
 
         _controller.Move(movementDirection * _playerSpeed * Time.deltaTime);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "OrganicTrash":
+                Debug.Log("Organic Trash Collected");
+                organicAmmo++;
+                break;
+            case "PlasticTrash":
+                Debug.Log("Plastic Trash Collected");
+                plasticAmmo++;
+                break;
+            case "MetallicTrash":
+                Debug.Log("Metallic Trash Collected");
+                metalAmmo++;
+                break;
+            default:
+                break;
+        }
+        other.gameObject.SetActive(false);
+        Destroy(other.gameObject);
     }
 }
